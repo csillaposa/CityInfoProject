caf9d26b41fd4575152129a08a0c449bce3f849f
Chily <cpo@appvestor.com>
2023-03-01 14:50:50 +0100

    Creating entities

---
+++ Entities/PointOfInterest.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        // we want to signify the relation btw City and Point of Interest
+        // navigation property => relationship will be created (foreign key will be the id of the city)
+        [ForeignKey("CityId")]
+        public City? City { get; set; }
+
+        //it is recommended to declare the foreign key property, but not required
+        public int CityId { get; set; }
+
+        public PointOfInterest(string name)
+        {
+            Name = name;
+        }
+    }
+}
