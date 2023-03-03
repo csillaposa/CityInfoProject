2220d5395f2af9ffacf1351bc684923047227cf2
Chily <cpo@appvestor.com>
2023-03-03 13:16:11 +0100

    Adding XML comments and making them show up in Swagger

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Serilog;
+using System.Reflection;
 using System.Text;
 
 // installed Serilog logging packages, need to configure
@@ -33,7 +34,13 @@ builder.Services.AddControllers(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setupAction =>
+{
+    var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+
+    setupAction.IncludeXmlComments(xmlCommentsFullPath);
+});
 
 // To inject a file extension content type
 builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
